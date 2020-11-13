5666213ab54109af80cff83c5087bdf333b92f1a
cuminal <cuminal@etud.insa-toulouse.fr>
2020-11-13 10:52:08 +0100

    mise a jour 2 de SimpleClasse

--- SimpleClasse.java
+++ SimpleClasse.java
@@ -10,6 +10,6 @@ public class SimpleClasse {
 		int a=3;
 		int b=5;
 		System.out.println("La somme de "+a+" et "+b+ " est : "+sc.somme(a, b));
-		System.out.println("petit changement");
+		System.out.println("petit changement 2");
 	}
 }
